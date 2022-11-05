fe05e8e5c02e590240905c63b4b668837a58b1a6
Zalupessa <little.mikle@yahoo.com>
2022-11-06 01:23:10 +0300

    first

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/LittleMikle/rest2.git/pkg/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
+	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
+	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
+
+	log.Println("API is running!!!")
+	http.ListenAndServe(":8081", router)
+
+}
